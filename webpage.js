e20df9a4a36b8f6181d6914f446899327e631e1b
rosaly26 <53266240+rosaly26@users.noreply.github.com>
2019-07-24 15:54:47 -0400

    Add files via upload

---
+++ webpage.js
@@ -0,0 +1,34 @@
+function openImage() {
+  // window.alert("ABCDEF");
+  var img= document.getElementById('lol');
+  var img1= document.getElementById('lol1');
+  img.style.display="block";
+  img1.style.display="block";
+  document.getElementById("lol2").innerHTML = "Hide this image.";
+}
+
+function hide() {
+  window.alert("hide");
+  var img= document.getElementById('lol');
+  var img1= document.getElementById('lol1');
+  img.style.display="none";
+  img1.style.display="none";
+  document.getElementById("lol2").innerHTML = "Show this image.";
+}
+
+function toggleImg() {
+  if(document.getElementById("lol2").innerHTML == "Show this image.") {
+    openImage();
+  }
+  else {
+      hide();
+  }
+}
+
+function expand() {
+
+}
+
+function unexpand() {
+  
+}
